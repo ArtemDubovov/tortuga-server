c79ccf203b7c5ae9fe93f5d728f545df43a87b1b
Artem Dubovov <artem.dubovov@bk.ru>
2022-11-15 17:43:21 +0300

    feat: add try for user and check code word

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,68 @@
 const ApiError = require('./../error/ApiError');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {User, Basket} = require('./../models/models');
+const { User, Basket } = require('./../models/models');
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role },
-  process.env.SECRET_KEY,
-  {expiresIn: '24h'}
+    process.env.SECRET_KEY,
+    { expiresIn: '24h' }
   );
 };
 
 class userController {
   async registration(req, res, next) {
-    const {email, password, role} = req.body;
-    if (!email || !password) {
-      return next(ApiError.badRequest('Не корректный email или пароль!'));
+    try {
+      const { email, password, role, code } = req.body;
+      if (!email || !password) {
+        return next(ApiError.badRequest('Не корректный email или пароль!'));
+      }
+      if (code !== process.env.SECRET_PASSWORD) {
+        return next(ApiError.badRequest('Не верный код!'));
+      }
+      const candidate = await User.findOne({ where: { email } });
+      if (candidate) {
+        return next(ApiError.badRequest('Пользователь с таким email уже зарегистрирован!'));
+      }
+      const hashPassword = await bcrypt.hash(password, 5);
+      const user = await User.create({ email, role, password: hashPassword });
+      const basket = await Basket.create({ userId: user.id });
+      const token = generateJwt(user.id, user.email, user.role);
+      return res.json(token);
+    } catch (e) {
+      return next(ApiError.badRequest('Что то пошло не так'));
     }
-    const candidate = await User.findOne({where: {email}});
-    if (candidate) {
-      return next(ApiError.badRequest('Пользователь с таким email уже зарегистрирован!'));
-    }
-    const hashPassword = await bcrypt.hash(password, 5);
-    const user = await User.create({email, role, password: hashPassword});
-    const basket = await Basket.create({userId: user.id});
-    const token = generateJwt(user.id, user.email, user.role);
-    return res.json(token);
   }
 
   async login(req, res, next) {
-    const {email, password} = req.body;
-    const user = await User.findOne({where: {email}});
-    if (!user) {
-      return next(ApiError.internal('Пользователь не найден.'))
+    try {
+      const { email, password, code } = req.body;
+      if (code !== process.env.SECRET_PASSWORD) {
+        return next(ApiError.badRequest('Не верный код!'));
+      }
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return next(ApiError.internal('Пользователь не найден.'))
+      }
+      let comparePassword = bcrypt.compareSync(password, user.password);
+      if (!comparePassword) {
+        return next(ApiError.internal('Указан не верный пароль.'));
+      }
+      const token = generateJwt(user.id, user.email, user.role);
+      return res.json({ token });
+    } catch (e) {
+      console.log(e);
+      return next(ApiError.badRequest('Что то пошло не так'));
     }
-    let comparePassword = bcrypt.compareSync(password, user.password);
-    if (!comparePassword) {
-      return next(ApiError.internal('Указан не верный пароль.'));
-    }
-    const token = generateJwt(user.id, user.email, user.role);
-    return res.json({token});
   }
 
   async check(req, res, next) {
-    const token = generateJwt(req.user.id, req.user.email, req.user.role);
-    return res.json({token});
+    try {
+      const token = generateJwt(req.user.id, req.user.email, req.user.role);
+      return res.json({ token });
+    } catch (e) {
+      return next(ApiError.badRequest('Что то пошло не так'));
+    }
   }
 }
 
