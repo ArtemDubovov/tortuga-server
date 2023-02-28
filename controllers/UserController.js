80be4a66d0bc32eac3c9224bfe15f92ac9ffe5fb
Artem <artem.dubovov@bk.ru>
2023-03-01 00:08:21 +0300

    feat: добавлен контроллер userController

---
+++ controllers/UserController.js
@@ -0,0 +1,51 @@
+class UserController {
+  async registration (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async login (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async logout (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async refreshToken (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAll (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getOne (req, res, next) {
+    try {
+
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+export default new UserController();
