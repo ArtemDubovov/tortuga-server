c4df3166cbda12610e871d7bb562f024f77c6cd3
Artem <artem.dubovov@bk.ru>
2023-03-01 11:29:57 +0300

    feat: добавлен tokenService и добавлен параметр isActivation в userDBO.

--- DBO/userDbo.js
+++ DBO/userDbo.js
@@ -1,9 +1,10 @@
 export class UserDBO {
-  constructor({email, avatar, about, role, _id}) {
+  constructor({email, avatar, about, role, _id, isActivation}) {
     this._id = _id;
     this.email = email;
-    this.avatar = avatar;
-    this.about = about;
+    this.avatar = avatar || null;
+    this.about = about || null;
     this.role = role;
+    this.isActivation = isActivation;
   }
 }
---
+++ services/tokenService.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { ApiError } from '../excepted/ApiError';
+
+const {JWT_ACCESS_SECRET, JWT_REFRESH_SECRET} = dotenv.config().parsed;
+
+const getTokens = (data) => {
+  const acessToken = jwt.sign(data, JWT_ACCESS_SECRET, {expiresIn: '1d'});
+  const refreshToken = jwt.sign(data, JWT_REFRESH_SECRET, {expiresIn: '30d'});
+  return {
+    acessToken,
+    refreshToken
+  }
+}
+
+const checkValidateToken = (token) => {
+  try {
+    const isValidate = jwt.verify(token, JWT_ACCESS_SECRET);
+    if (isValidate) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    throw ApiError.Unauthorization('Нет прав для доступа.');
+  }
+}
+
+export {
+  getTokens,
+  checkValidateToken
+}
