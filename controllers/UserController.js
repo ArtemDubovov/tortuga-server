21fe307e3bb0c602b9c8530d85c064cec80dbb3e
Artem <artem.dubovov@bk.ru>
2023-03-13 13:54:19 +0300

    feat: обновлены куки

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -26,7 +26,6 @@ class UserController {
       res.cookie('refreshToken', userData.tokens.refreshToken,  {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: '/',
-        secure: true, 
         sameSite: 'none'
       });
       res.json(userData);
